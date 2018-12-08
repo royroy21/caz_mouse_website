4eb2e4e9e21d1f3825c1053ce60327feb37acf1c
Roy Hanley <royhanley8@gmail.com>
2018-12-08 18:04:47 +0000

    detect mobile

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request, send_from_directory
+from user_agents import parse
 
 app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/', methods=['GET'])
 def home():
+    http_user_agent_string = request.environ['HTTP_USER_AGENT']
+    user_agent = parse(http_user_agent_string)
+
+    if user_agent.is_mobile:
+        return render_template('mobile_error.html')
+
     return render_template('home.html')
 
 
