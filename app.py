e3b54eb167f480695d6162b793fe1a371cd26e1e
Roy Hanley <royhanley8@gmail.com>
2018-12-08 14:09:37 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/download/<path:filename>', methods=['GET'])
+def download(filename):
+    return send_from_directory(directory='programs', filename=filename)
+
+
+if __name__ == '__main__':
+    app.run()
